1953489a02915c1b136815ce7f615eb225872bbc
navallur-smitha <smitha.navallur@gmail.com>
2022-10-05 18:03:43 +0530

    port fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const express = require('express')
 const app = express()
-const port = 8000
+//const port = 8000
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 })
 
-app.listen(port, () => {
+.listen(process.env.PORT || 5000, () => {
   console.log(`App running on port ${port}.`)
 })
